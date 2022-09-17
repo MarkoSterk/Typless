4de471edf8bd402595d24d329a717d28131b89b8
Marko Sterk <marko_sterk@hotmail.com>
2022-09-17 09:01:48 +0200

    Added compression to app responses - Flask-Compress

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import default_exceptions
+from flask_compress import Compress
 
 from app.config import Config
 from app.controllers.errorController import error_response
@@ -10,12 +11,14 @@ It needs a Config object which is located in the same folder (config.py)
 """
 
 db = SQLAlchemy() ##SQLalchemy instance for database connection and functionality
+compress = Compress() ##uses default settings
 
 def create_app(config_class: object=Config):
     app=Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+    compress.init_app(app)
 
     # from app.models.data import Data
 
