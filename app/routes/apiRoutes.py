da6f49b25721acc5f9b275f26c540459f09cbcbb
Marko Sterk <marko_sterk@hotmail.com>
2022-09-18 14:32:26 +0200

    Added url_prefix to API routes blueprint

--- app/routes/apiRoutes.py
+++ app/routes/apiRoutes.py
@@ -4,30 +4,30 @@ from app.controllers.apiController import (process_data, save_data,
                                             delete_data_one)
 
 
-apiRoutes = Blueprint('apiRoutes', __name__)
+apiRoutes = Blueprint('apiRoutes', __name__, url_prefix='/api/v1/data')
 
 #data processing endopint. Calls process_data API controller
-@apiRoutes.route('/api/v1/data/process', methods=["POST"])
+@apiRoutes.route('/process', methods=["POST"])
 def process():
     return process_data()
 
 #save data endopint. Calls save_data API controller
-@apiRoutes.route('/api/v1/data/save', methods=["POST"])
+@apiRoutes.route('/save', methods=["POST"])
 def save():
     return save_data()
 
 #get all records endpoint. Calls get_data_all API controller
-@apiRoutes.route('/api/v1/data/get_all', methods=["GET"])
+@apiRoutes.route('/get_all', methods=["GET"])
 def get_all():
     return get_data_all()
 
 #get one record via id. Calls get_data_one API controller
-@apiRoutes.route('/api/v1/data/get/<int:id>', methods=["GET"])
+@apiRoutes.route('/get/<int:id>', methods=["GET"])
 def get_one(id: int):
     return get_data_one(id)
 
 #delete one record via id. Calls delete_data_one API controller
-@apiRoutes.route('/api/v1/data/delete/<int:id>', methods=["DELETE"])
+@apiRoutes.route('/delete/<int:id>', methods=["DELETE"])
 def delete_one(id: int):
     return delete_data_one(id)
 
