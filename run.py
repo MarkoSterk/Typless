426ba96f5e91ecf8e96ac98a00d25ac34dbd2b8c
Marko Sterk <marko_sterk@hotmail.com>
2022-11-07 09:21:54 +0100

    Minor change: removed pywebview from run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import create_app
-import webview
 
 app=create_app()
 
@@ -7,7 +6,5 @@ debug = True
 port = 3000
 
 if __name__ == '__main__':
-    #app.run(debug=debug, port=port)
-    #app.run()
-    webview.create_window('Typless', app, width=800, height=480)
-    webview.start()
+    app.run(debug=debug, port=port)
+    app.run()
